d515f0634ec9985d3bf413865b4ef357b86c5bd8
masedie <masedie@masedies-MacBook-Air.local>
2024-04-12 16:17:44 +0200

    Yawn

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,10 +4,10 @@
 const firstArg = process.argv[2];
 
 if (isNaN(Number(firstArg))) {
-  console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 } else {
   const numOccurrences = parseInt(firstArg);
   for (let i = 0; i < numOccurrences; i++) {
-    console.log("C is fun");
+    console.log('C is fun');
   }
 }
