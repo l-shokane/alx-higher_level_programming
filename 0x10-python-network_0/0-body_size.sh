fec843851ab5228e3088d6dbc5814bd86970194b
l-shokane <masedie.rev@gmail.com>
2024-05-01 10:24:07 -0700

    Heyy

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Script that takes in, sends a request and
-# displays the response of a URL.
-
+# Script that takes in, sends a request and displays the response of URL.
 curl -sI $1 | grep "Content-Length" | cut -d " " -f2
